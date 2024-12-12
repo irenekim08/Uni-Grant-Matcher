5f680bd6acdcd48d47523e48e962c079803ad001
irenekim08 <ir.9ene11@gmail.com>
2024-12-11 22:00:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-pages/
-    faculty.py
-    student.py
-    
-
 st.title("Uni Grant Matcher")
 st.write(
     "In need of funding but don't know what grant to apply for? Use the Uni Grant Matcher to find your best fit!"
