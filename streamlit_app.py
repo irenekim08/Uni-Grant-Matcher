2a44d22f1fc68ffacb8e625ceb03fa00a697bf09
irenekim08 <ir.9ene11@gmail.com>
2024-12-11 22:10:40 -0600

    Update streamlit_app.py association selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,11 @@ st.write(
     "In need of funding but don't know what grant to apply for? Use the Uni Grant Matcher to find your best fit!"
 )
 
-f, s = st.columns(2)
-association_f.link_button("Faculty", 
+association = st.selectbox(
+    "What is the applicant's association with Uni?",
+    ("Faculty", "Student"),
+    index = None,
+    placeholder = "Select association..."
+)
+
+st.write("Displaying grants uses for:", association)
