b2cc0f4a0b3dfeade8fe90862e219ae41bba8ea3
irenekim08 <ir.9ene11@gmail.com>
2024-12-12 23:29:13 -0600

    Update streamlit_app.py trying taking out while loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-
-@st.cache_data
-def long_running_function(param1, param2):
-    return(
-
 st.title("Uni Grant Matcher")
 st.write(
     "In need of funding but don't know what grant to apply for? Use the Uni Grant Matcher to find your best fit!"
@@ -87,9 +82,8 @@ def generate_results(purposes: list) -> list:
 
 
 # Generate results while purposes != empty
-while purposes != []:
+#while purposes != []:
     st.write(f"Check out: {generate_results(purposes)}")
-    )
 
          
          
