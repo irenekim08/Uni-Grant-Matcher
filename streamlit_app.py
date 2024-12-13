75057302e8abbe34222e450a956a74a234056ca0
irenekim08 <ir.9ene11@gmail.com>
2024-12-12 23:01:32 -0600

    Update streamlit_app.py swap grant keys and vals

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,36 +14,63 @@ association = st.selectbox(
 
 st.write("Displaying grant uses for:", association)
 
-# Pill buttons for each association
+# Faculty pill buttons
 if association == "Faculty":
-    list_purposes_f = ["Special Opportunity (Non-Uni)", "Conference", "Research/Publication", 
-                  "Activity", "Travel/Housing/Food", "Insurance", "Project Supplies",
-                  "Classroom Supplies", "Startup (New program)", "Professional Membership"]
-    purposes_f = st.pills("Select all purposes for grant", list_purposes_f, selection_mode = "multi")
+    
+    # Dictionary containing all grants with their corresponding purposes
+    grants = {"Classroom Supplies": "Uni High Faculty Classroom Needs & Projects Request",
+              "Special Opportunity (Non-Uni)": ["Ang Current Use Fund (Professional Development Funding)", "Uni Endowment Fund (Professional Development Funding)"],
+              "Conference": "Ang Current Use Fund (Professional Development Funding)",
+              "Research/Publication": "Frankel Fund For Learning Innovation", "Innovations in Learning (Professional Development Funding)", "Teaching Excellence (Makino Awards)"],
+              "Activity": ["Innovations in Learning (Professional Development Funding)", "Teaching Excellence (Makino Awards)"],
+              "Project Supplies": ["Innovations in Technology (Professional Development Funding)", "Uni Endowment Fund (Professional Development Funding)", "Teaching Excellence (Makino Awards)"]
+              "Startup (New program)": ["Uni Endowment Fund (Professional Development Funding)", "Teaching Excellence (Makino Awards)"],
+              "Professional Membership": "Uni Endowment Fund (Professional Development Funding)",
+              "Travel/Housing/Food": "Teaching Excellence (Makino Awards)"
 
-    faculty_grants = {"Uni High Faculty Classroom Needs & Projects Request": "Classroom Supplies",
-                "Ang Current Use Fund (Professional Development Funding)": ["Special Opportunity (Non-Uni)", "Conference"],
-                "Frankel Fund For Learning Innovation": "Research",
-                "Innovations in Technology (Professional Development Funding)": ["Research", "Activity", "Project Supplies"],
-                "Uni Endowment Fund (Professional Development Funding)": ["Special Opportunity (Non-Uni)", "Project Supplies", "Startup (New program)", "Professional Membership"],
-                "Teaching Excellence (Makino Awards)": ["Research/Publication", "Activity", "Travel/Housing/Food", "Project Supplies", "Startup (New program)"]}
+    purposes_f = st.pills("Select all purposes for grant", grants.keys(), selection_mode = "multi")
+
+
 
 
                                                         
+# Student pill buttons    
+if association == "Student":  
+
+    # Dictionary containing all grants with their corresponding purposes
+    grants = {"Conference": "Barbara Lazarus Memorial Fund",
+              "Research": ["Barbara Lazarus Memorial Fund", "Frankel Fund For Learning Innovation"]
+              "Activity": ["Barbara Lazarus Memorial Fund", "Makino Awards"]
+              "Travel/Housing/Food": ["Barbara Lazarus Memorial Fund", "Boren Scholarship", "Makino Awards"]
+              "Supplies": ["Barbara Lazarus Memorial Fund", "Boren Scholarship", "Makino Awards"]
+              "Scholarship/Special Opportunity": ["Boren Scholarship", "Eastern Illini Electric Cooperative", "The Illinois Odd Fellow-Rebekah Scholarship Program", "Martin Luther King Scholarship"]
+              "Study Abroad": ["Boren Scholarship", "McNevin Scholarship"]
+              "AP/ACT Prep": ["Boren Scholarship", "McNevin Scholarship"]
+              "Camps (academic, arts, athletics)": ["Boren Scholarship", "McNevin Scholarship"]
+
+    purposes_s = st.pills("Select all purposes for grant", grants.keys(), selection_mode = "multi")  
+
+
+
+
+
+st.write(f"Check out: {generate_results}")
+
+# Generate results while purposes != empty
+def generate_results(purposes: list) -> list:
+    """
+    Generates results of suitable grant(s) based off of user's chosen purposes.
+
+    Args:
+            purposes(list):        List containing strings of chosen purpose options.
     
-if association == "Student":
-    list_purposes_s = ["Scholarship/Special Opportunity (Non-Uni)", "Conference", "Research", 
-                  "Activity", "Travel/Housing/Food", "Supplies", "Study Abroad", "AP/ACT Prep",
-                  "Startup (New program)", "Camps (academic, arts, athletics)"]
-    purposes_s = st.pills("Select all purposes for grant", list_purposes_s, selection_mode = "multi")    
+    Returns:
+            matches(list):         List containing string titles of matched grants.
+    """
 
-    student_grants = {"Barbara Lazarus Memorial Fund": ["Conference", "Research", "Activity", "Travel/Housing/Food", "Supplies"],
-                "Boren Scholarship": ["Scholarship/Special Opportunity", "Travel/Housing/Food", "Supplies", "Study Abroad", "AP/ACT Prep", "Camps (academic, arts, athletics)"],
-                "Eastern Illini Electric Cooperative": "Scholarship/Special Opportunity",
-                "Makino Awards": ["Activity", "Travel/Housing/Food", "Supplies"],
-                "The Illinois Odd Fellow-Rebekah Scholarship Program": "Scholarship/Special Opportunity",
-                "Frankel Fund For Learning Innovation": "Research",
-                "McNevin Scholarship": ["Study Abroad", "AP/ACT Prep", "Camps(academic, arts, athletics)"],
-                "Martin Luther King Scholarship": "Scholarship/Special Opportunity"}
+    for purpose in purposes:
+        
 
-# Generate results
+
+    
+    
