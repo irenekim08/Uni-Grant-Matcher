aa4282e2e61381b3d965d66c082e13b31a913dee
irenekim08 <ir.9ene11@gmail.com>
2024-12-18 09:45:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ if purposes != []:
     for grant in set(generate_results(purposes)):
         st.link_button(f"Apply for {grant}", grant_links[grant])
 
-    st.write("For more information on grants and funds at Uni, visit the [Uni High website.](%s)" % uni_website_url)
-    st.markdown("For more information on grants and funds at Uni, visit the [Uni High website.](%s)" % uni_website_url)         
+    st.write("For more information on grants and funds at Uni, visit the [Uni High website.](%s)" %uni_website_url)
+    st.markdown("For more information on grants and funds at Uni, visit the [Uni High website.](%s)" %uni_website_url)         
     
     
